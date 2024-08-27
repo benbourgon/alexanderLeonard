05f436adfaea3d4a8bafd4996e64b0eed5bf0b7f
Ben Bourgon <benjaminbourgon@gmail.com>
2024-08-27 00:21:00 -0400

    Add netlift

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,14 +7,5 @@ import react from "@astrojs/react";
 import { sanityConfig } from "./src/config/sanity.config";
 export default defineConfig({
   site: "https://example.com",
-  integrations: [
-    sitemap(),
-    tailwind(),
-    sanity({
-      projectId: SANITY_PROJECT_ID,
-      dataset: SANITY_DATASET,
-      useCdn: false,
-    }),
-    react(),
-  ],
+  integrations: [sitemap(), tailwind(), sanity(sanityConfig), react()],
 });
